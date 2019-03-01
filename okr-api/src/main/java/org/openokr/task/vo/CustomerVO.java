9dc33f8eda17d94354b7c19ba142fcc8bc0a75bf
linaer <aelin@onylou.com>
2019-03-01 14:37:38 +0800

    实体修改提交

---
+++ okr-api/src/main/java/org/openokr/task/vo/CustomerVO.java
@@ -0,0 +1,166 @@
+package org.openokr.task.vo;
+
+import com.zzheng.framework.base.vo.BaseVO;
+import java.io.Serializable;
+import java.util.Date;
+
+public class CustomerVO extends BaseVO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+    /**
+     * 主键
+     */
+    private String id;
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 状态
+     */
+    private String status;
+
+    /**
+     * 备注
+     */
+    private String remark;
+
+    /**
+     * 创建者
+     */
+    private String createUserId;
+
+    /**
+     * 创建时间
+     */
+    private Date createTs;
+
+    /**
+     * 更新者
+     */
+    private String updateUserId;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTs;
+
+
+
+    /**
+     * 主键
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * 主键
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * 名称
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 名称
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 状态
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * 状态
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    /**
+     * 备注
+     */
+    public String getRemark() {
+        return remark;
+    }
+
+    /**
+     * 备注
+     */
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    /**
+     * 创建者
+     */
+    public String getCreateUserId() {
+        return createUserId;
+    }
+
+    /**
+     * 创建者
+     */
+    public void setCreateUserId(String createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    /**
+     * 创建时间
+     */
+    public Date getCreateTs() {
+        return createTs;
+    }
+
+    /**
+     * 创建时间
+     */
+    public void setCreateTs(Date createTs) {
+        this.createTs = createTs;
+    }
+
+    /**
+     * 更新者
+     */
+    public String getUpdateUserId() {
+        return updateUserId;
+    }
+
+    /**
+     * 更新者
+     */
+    public void setUpdateUserId(String updateUserId) {
+        this.updateUserId = updateUserId;
+    }
+
+    /**
+     * 更新时间
+     */
+    public Date getUpdateTs() {
+        return updateTs;
+    }
+
+    /**
+     * 更新时间
+     */
+    public void setUpdateTs(Date updateTs) {
+        this.updateTs = updateTs;
+    }
+
+}
