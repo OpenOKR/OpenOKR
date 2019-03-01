87bbdf8787b8b196bcf0f30794eff7afaf91a4ce
linaer <aelin@onylou.com>
2019-03-01 16:57:49 +0800

    任务管理控制器使用vo创建

--- okr-api/src/main/java/org/openokr/task/request/TaskInfoVO.java
+++ okr-api/src/main/java/org/openokr/task/request/TaskInfoVO.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class TaskInfoVO extends BaseVO implements Serializable {
@@ -27,5 +28,23 @@ public class TaskInfoVO extends BaseVO implements Serializable {
 
     @ApiModelProperty(value = "创建时间")
     private Date createTs;
+
+    @ApiModelProperty(value = "任务开始时间")
+    private Date taskStartTime;
+
+    @ApiModelProperty(value = "任务结束时间")
+    private Date taskEndTime;
+
+    @ApiModelProperty(value = "JIRA标签(编码)")
+    private String jiraLabel;
+
+    @ApiModelProperty(value = "创建者ID")
+    private String createUserId;
+
+    @ApiModelProperty(value = "创建者名称")
+    private String createUserName;
+
+    @ApiModelProperty(value = "KR信息")
+    private List<KrInfoVO> krInfoVOS;
 
 }
