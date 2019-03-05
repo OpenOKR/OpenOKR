cdfaf67ef523b65c9794ce33974056e2b6b200f5
linaer <aelin@onylou.com>
2019-03-05 17:11:21 +0800

    接口登录开发

--- okr-web/src/main/java/org/openokr/application/web/LoginController.java
+++ okr-web/src/main/java/org/openokr/application/web/LoginController.java
@@ -1,11 +1,14 @@
 package org.openokr.application.web;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.util.WebUtils;
-import org.openokr.application.shiro.ShiroConfiguration;
-import org.openokr.application.shiro.filter.FormAuthenticationFilter;
 import org.openokr.application.shiro.AuthRealm;
 import org.openokr.application.shiro.RSAService;
+import org.openokr.application.shiro.ShiroConfiguration;
+import org.openokr.application.shiro.UsernamePasswordToken;
+import org.openokr.application.shiro.filter.FormAuthenticationFilter;
 import org.openokr.utils.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -111,5 +114,36 @@ public class LoginController extends BaseController {
         ex.put("modulus", Base64.encodeBase64String(rsaPublicKey.getModulus().toByteArray()));
         ex.put("exponent", Base64.encodeBase64String(rsaPublicKey.getPublicExponent().toByteArray()));
         return ex;
+    }
+
+
+    /**
+     * 登录失败
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping(value = "/devLogin.json",method = RequestMethod.GET)
+    @ResponseBody
+    public String devLogin(HttpServletRequest request, HttpServletResponse response,String username) {
+        //得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        char[] passwordArr = new char[]{'1','2','3','4','5','6'};
+        UsernamePasswordToken token = new UsernamePasswordToken(
+                username,  //登录用户名
+                passwordArr,false,"0:0:0:0:0:0:0:1",null,false); //登录密码
+/*        token.setRememberMe(false);
+        token.setHost("0:0:0:0:0:0:0:1");*/
+        try{
+            //登录，即身份验证
+            subject.login(token);
+        }catch (Exception e){
+            //登录失败
+            System.out.println(e);    //打印错误信息，测试阶段，找问题比较方便
+            System.out.println("用户信息验证失败");
+            token.clear();
+            return "error";  //这里是登录失败的页面
+        }
+        return request.getSession().getId();
     }
 }
